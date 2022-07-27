2bdb5adb1da64a2578d90aac745965ddc6a38ade
daianasg1 <daianasoledadg@hotmail.com>
2022-07-27 10:57:30 -0300

    se creo el proyecto backend
    y se creo la carpeta backEnd

---
+++ miPortafolio/backEnd/daianagodoy/src/test/java/com/portfolio/daianagodoy/DaianagodoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.daianagodoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaianagodoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
