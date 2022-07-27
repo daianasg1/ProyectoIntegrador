0739473c594422e520a929e9236ce9d403c0355e
daianasg1 <daianasoledadg@hotmail.com>
2022-07-27 15:08:14 -0300

    back

---
+++ miPortafolio/backEnd/daianagodoyBACK/daianagodoyBACK/src/main/java/com/portfolio/daianagodoyBACK/Controller/PersonaController.java
@@ -0,0 +1,44 @@
+package com.portfolio.daianagodoyBACK.Controller;
+
+import com.portfolio.daianagodoyBACK.Entity.Persona;
+import com.portfolio.daianagodoyBACK.Interface.IPersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PersonaController {
+    @Autowired
+    IPersonaService ipersonaService;
+
+    @GetMapping("personas/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada correctamente";
+    }
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "La persona fue eliminada correctamente";
+    }
+    // URL:PUERTO/personas/editar/4/nombre & apellido & img
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
+}
